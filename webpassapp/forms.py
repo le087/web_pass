5bcc3dd0843bb8a72bbf59880025b307bc294031
Mak Tomilov <mak.tomilov@gmail.com>
2012-10-03 00:58:03 +0600

    Add japanese and France language

--- webpassapp/forms.py
+++ webpassapp/forms.py
@@ -4,11 +4,12 @@ from django import forms
 
 NUM_VARIANTS = [(i+1, i+1) for i in range(20)]
 LENTH_PASS = [(i+1, i+1) for i in range(20)]
-LANG = [('italian', 'Итальянский'), ('france','Французский'), ('japan', 'Японский')]
+LANG = [('italian', 'Итальянский'), ('french','Французский'), ('japanese', 'Японский')]
 
 class SetPassForm(forms.Form):
+    language = forms.ChoiceField(label='Язык пароля', widget=forms.Select, choices=LANG)
     num_pass = forms.ChoiceField(label='Количество паролей', widget=forms.Select, choices=NUM_VARIANTS)
     lenth_pass = forms.ChoiceField(label='Длина пароля', widget=forms.Select, choices=LENTH_PASS)
     upper_pass = forms.BooleanField(label='Прописные буквы', required=False)
     string_replace_num = forms.BooleanField(label='Использовать цифры', required=False)
-    language = forms.ChoiceField(label='Язык пароля', widget=forms.Select, choices=LANG)
+
